2be40efbcf5298538a59810cf930b395b9725ad1
Akshit Saini <115421818+akshit2941@users.noreply.github.com>
2024-03-20 21:10:51 +0530

    Update firebase.js

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB54DCk_c9fxXNW67qcdMpz1RzC3qGYsuY",
-  authDomain: "eventral-47e8e.firebaseapp.com",
-  projectId: "eventral-47e8e",
-  storageBucket: "eventral-47e8e.appspot.com",
-  messagingSenderId: "297141149658",
-  appId: "1:297141149658:web:a0356ec447857f590feae3",
-  measurementId: "G-PBXCQT32JT"
+  apiKey: "Your API Here",
+  authDomain: "Your API Here",
+  projectId: "Your API Here",
+  storageBucket: "Your API Here",
+  messagingSenderId: "Your API Here",
+  appId: "Your API Here",
+  measurementId: "Your API Here"
 };
 
 const app = initializeApp(firebaseConfig);
